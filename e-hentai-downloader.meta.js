2d957137ffe21c0d693072dac5db8190c2d82a2d
ccloli <h@ccloli.com>
2015-12-27 20:17:15 +0800

    Scale js files

---
+++ e-hentai-downloader.meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         E-Hentai Downloader
+// @version      1.18.6
+// @description  Download E-Hentai archive as zip file
+// @author       864907600cc
+// @icon         https://secure.gravatar.com/avatar/147834caf9ccb0a66b2505c753747867
+// @match        http://exhentai.org/g/*
+// @match        http://g.e-hentai.org/g/*
+// @match        http://r.e-hentai.org/g/*
+// @include      http://exhentai.org/g/*
+// @include      http://g.e-hentai.org/g/*
+// @include      http://r.e-hentai.org/g/*
+// @namespace    http://ext.ccloli.com
+// @updateURL    https://github.com/ccloli/E-Hentai-Downloader/raw/master/e-hentai-downloader.meta.js
+// @downloadURL  https://github.com/ccloli/E-Hentai-Downloader/raw/master/e-hentai-downloader.user.js
+// @supportURL   https://github.com/ccloli/E-Hentai-Downloader/issues
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM_info
+// ==/UserScript==
---
+++ src/first.js
@@ -0,0 +1,53 @@
+
+// This script using JSZip & FileSaver.js
+
+'use strict';
+
+console.log('[EHD] E-Hentai Downloader is running.');
+console.log('[EHD] Bugs Report >', 'https://github.com/ccloli/E-Hentai-Downloader/issues | https://greasyfork.org/scripts/10379-e-hentai-downloader/feedback');
+console.log('[EHD] To report a bug, showing all the "[EHD]" logs is wonderful. =w=');
+
+// Opera 12- (Presto) doesn't support generating blob url, and if generate as data url, it may cause crashes.
+if (navigator.userAgent.indexOf('Presto') >= 0) {
+	alert('Your Opera doesn\'t support E-Hentai Downloader. You need to update it to Opera 15+.');
+	console.error('[EHD] Opera 12- (Presto) doesn\'t support E-Hentai Downloader. UserAgent > ' + navigator.userAgent);
+}
+
+// Remove IE support
+else if (navigator.userAgent.indexOf('Trident') >= 0) {
+	alert('Your browser doesn\'t support E-Hentai Downloader. You need to switch to other browsers.');
+	console.error('[EHD] IE doesn\'t support E-Hentai Downloader. UserAgent > ' + navigator.userAgent);
+}
+
+// Simple fixed not working on Violentmonkey
+// Not sure why these GM_* APIs are working now... OTL
+/*
+else if ((navigator.userAgent.indexOf('OPR') >= 0 || navigator.userAgent.indexOf('Maxthon') >= 0) && !GM_info) {
+	var GM_getValue = GM_getValue || function(i) {
+		return localStorage.getItem(i);
+	}
+	var GM_setValue = GM_setValue || function(i, j) {
+		return localStorage.setItem(i, j);
+	}
+	var GM_info = GM_info || {script: {}, scriptHandler: 'Violentmonkey'};
+	//var unsafeWindow = window;
+}
+*/
+
+// GreaseMonkey 3.2 beta 1 and older version can't load content of GM_xhr.response, and this can't be fix.
+else if (
+	(!GM_info.scriptHandler || GM_info.scriptHandler.indexOf('GreaseMonkey') >= 0) && GM_info.version != null && (
+		GM_info.version.split('.')[0] - 0 < 3 || (
+			GM_info.version.split('.')[0] - 0 == 3 && (
+				GM_info.version.split('.')[1].split('beta')[0] - 0 <= 2 &&
+				GM_info.version.indexOf('beta') >= 0 && 
+				GM_info.version.split('beta')[1] - 0 < 2
+			)
+		)
+	)
+) {
+	alert('Your GreaseMonkey doesn\'t support E-Hentai Downloader. The first supported version is GreaseMonkey 3.2 beta 2. Please update your GreaseMonkey to enjoy. =w=');
+	console.error('[EHD] GreaseMonkey doesn\'t support E-Hentai Downloader. GreaseMonkey Version > ' + GM_info.version);
+}
+
+// GreasyFork doesn't allow obfuscated or minified script, so if you want to see the main function, please skip to line 9510
