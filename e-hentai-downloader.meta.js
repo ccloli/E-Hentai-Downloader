e6801df14c4f7d559dbf7f2e62d1e81b02701c42
ccloli <h@ccloli.com>
2016-02-01 16:10:56 +0800

    added @connect to fix the warning of request cross-origin resource on Tampermonkey
    For more information, see Tampermonkey documentation ( http://tampermonkey.net/documentation.php#_connect ) or EHD wiki about this ( https://github.com/ccloli/E-Hentai-Downloader/wiki/Cross-origin-request-warning-from-Tampermonkey )

--- e-hentai-downloader.meta.js
+++ e-hentai-downloader.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         E-Hentai Downloader
-// @version      1.18.7
+// @version      1.18.8
 // @description  Download E-Hentai archive as zip file
 // @author       864907600cc
 // @icon         https://secure.gravatar.com/avatar/147834caf9ccb0a66b2505c753747867
@@ -14,6 +14,7 @@
 // @updateURL    https://github.com/ccloli/E-Hentai-Downloader/raw/master/e-hentai-downloader.meta.js
 // @downloadURL  https://github.com/ccloli/E-Hentai-Downloader/raw/master/e-hentai-downloader.user.js
 // @supportURL   https://github.com/ccloli/E-Hentai-Downloader/issues
+// @connect      *
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_xmlhttpRequest
