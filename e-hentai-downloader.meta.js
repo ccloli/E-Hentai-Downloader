7b4e525890e78aeff26c86050a56a25855336f4c
ccloli <h@ccloli.com>
2016-07-27 11:42:42 +0800

    Supported HTTPS (also removed duplicate @match metadata)

--- e-hentai-downloader.meta.js
+++ e-hentai-downloader.meta.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         E-Hentai Downloader
-// @version      1.22
+// @version      1.22.1
 // @description  Download E-Hentai archive as zip file
 // @author       864907600cc
 // @icon         https://secure.gravatar.com/avatar/147834caf9ccb0a66b2505c753747867
-// @match        http://exhentai.org/g/*
-// @match        http://g.e-hentai.org/g/*
-// @match        http://r.e-hentai.org/g/*
 // @include      http://exhentai.org/g/*
 // @include      http://g.e-hentai.org/g/*
 // @include      http://r.e-hentai.org/g/*
+// @include      https://exhentai.org/g/*
+// @include      https://g.e-hentai.org/g/*
+// @include      https://r.e-hentai.org/g/*
 // @namespace    http://ext.ccloli.com
 // @updateURL    https://github.com/ccloli/E-Hentai-Downloader/raw/master/e-hentai-downloader.meta.js
 // @downloadURL  https://github.com/ccloli/E-Hentai-Downloader/raw/master/e-hentai-downloader.user.js
