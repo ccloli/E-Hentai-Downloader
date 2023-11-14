f5b99e6fbbedff68f41ffcc79fefa8d9af790135
Sorawit Jaroenbenjawong <pavawit@gmail.com>
2023-11-14 17:50:28 +0700

    run build

--- e-hentai-downloader.user.js
+++ e-hentai-downloader.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         E-Hentai Downloader
-// @version      1.35.2
+// @name         E-Hentai Downloader (Fork)
+// @version      2.0.1
 // @description  Download E-Hentai archive as zip file
-// @author       864907600cc
+// @author       Noone
 // @icon         https://secure.gravatar.com/avatar/147834caf9ccb0a66b2505c753747867
 // @include      http://exhentai.org/g/*
 // @include      http://e-hentai.org/g/*
@@ -14,8 +14,8 @@
 // @include      https://g.e-hentai.org/g/*
 // @include      https://r.e-hentai.org/g/*
 // @namespace    http://ext.ccloli.com
-// @updateURL    https://github.com/ccloli/E-Hentai-Downloader/raw/master/e-hentai-downloader.meta.js
-// @downloadURL  https://github.com/ccloli/E-Hentai-Downloader/raw/master/e-hentai-downloader.user.js
+// @updateURL    https://github.com/penguin-jedi/E-Hentai-Downloader/raw/feature/image-name-apply-templates/e-hentai-downloader.meta.js
+// @downloadURL  https://github.com/penguin-jedi/E-Hentai-Downloader/raw/feature/image-name-apply-templates/e-hentai-downloader.user.js
 // @supportURL   https://github.com/ccloli/E-Hentai-Downloader/issues
 // @connect      e-hentai.org
 // @connect      exhentai.org
@@ -12486,8 +12486,9 @@ function getSafeName(str, ignoreSlash) {
 }
 
 // replace dir name and zip filename
-function getReplacedName(str) {
+function getReplacedName(str, imageNumber = '') {
 	return replaceHTMLEntites(str.replace(/\{gid\}/gi, unsafeWindow.gid)
+		.replace(/\{imageNumber\}/gi, imageNumber)
 		.replace(/\{token\}/gi, unsafeWindow.token)
 		.replace(/\{title\}/gi, getSafeName(document.getElementById('gn').textContent))
 		.replace(/\{subtitle\}/gi, document.getElementById('gj').textContent ? getSafeName(document.getElementById('gj').textContent) : getSafeName(document.getElementById('gn').textContent))
@@ -12520,7 +12521,12 @@ function renameImages() {
 				}
 			}
 		}
-		else elem['imageName'] = elem['imageNumber'] + (setting['number-separator'] ? setting['number-separator'] : '：') + elem['imageName'];
+		else {
+			var numberSeparator = setting['number-separator'] ? setting['number-separator'] : '{imageNumber}：';
+			var backwardCompatibleCheck = numberSeparator.includes('{imageNumber}');
+			numberSeparator = backwardCompatibleCheck ? numberSeparator : '{imageNumber}' + numberSeparator;
+			elem['imageName'] = getReplacedName(numberSeparator + elem['imageName'], elem['imageNumber']);
+		}
 	});
 }
 
@@ -14417,7 +14423,7 @@ function showSettings() {
 					<div class="g2"><label><input type="checkbox" data-ehd-setting="speed-detect"> Abort downloading current image if speed is less than <input type="number" data-ehd-setting="speed-min" min="0" placeholder="5" style="width: 46px;"> KB/s in <input type="number" data-ehd-setting="speed-expired" min="1" placeholder="30" style="width: 46px;"> second(s)</label></div>\
 					<div class="g2"><label>Skip current image after retried <input type="number" data-ehd-setting="retry-count" min="1" placeholder="3" style="width: 46px;"> time(s)</label></div>\
 					<div class="g2"><label>Delay <input type="number" data-ehd-setting="delay-request" min="0" placeholder="0" step="0.1" style="width: 46px;"> second(s) before requesting next image</label></div>\
-					<div class="g2"><label><input type="checkbox" data-ehd-setting="number-images"> Number images (001：01.jpg, 002：01_theme.jpg, 003：02.jpg...) (Separator <input type="text" data-ehd-setting="number-separator" style="width: 46px;" placeholder="：">)</label></div>\
+					<div class="g2"><label><input type="checkbox" data-ehd-setting="number-images"> Number images (001：01.jpg, 002：01_theme.jpg, 003：02.jpg...)<br />(Separator <input type="text" data-ehd-setting="number-separator" style="width: 160px;" placeholder="{imageNumber}：">*) templates is also applied</label></div>\
 					<div class="g2"><label><input type="checkbox" data-ehd-setting="number-real-index"> Number images with original page number if pages range is set</label></div>\
 					<div class="g2"><label><input type="checkbox" data-ehd-setting="number-auto-retry"> Retry automatically when images download failed</label></div>\
 					<div class="g2"><label><input type="checkbox" data-ehd-setting="auto-download-cancel"> Get downloaded images automatically when canceled downloading</label></div>\
@@ -14744,7 +14750,7 @@ function toggleFilenameConfirmInput(hide){
 		extendNodes = document.createElement('div');
 		extendNodes.className = 'ehD-box-extend';
 		extendNodes.innerHTML = '<div class="g2">' + ehDownloadArrow + ' <a><label>File Name <input type="text" class="ehD-box-extend-filename"></label></a></div>' +
-		                        '<div class="g2">' + ehDownloadArrow + ' <a><label>Path Name <input type="text" class="ehD-box-extend-dirname"</label></a></div>';
+		                        '<div class="g2">' + ehDownloadArrow + ' <a><label>Path Name <input type="text" class="ehD-box-extend-dirname"></label></a></div>';
 		ehDownloadBox.appendChild(extendNodes);
 
 		dirName = getReplacedName(!setting['dir-name'] ? '{gid}_{token}' : setting['dir-name']);
