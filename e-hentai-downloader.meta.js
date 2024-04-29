bbd7a9a6a83946532b3c6d32500ed2d7efefc8e3
ccloli <h@ccloli.com>
2024-04-29 22:38:59 +0800

    Updated meta.js

--- e-hentai-downloader.meta.js
+++ e-hentai-downloader.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         E-Hentai Downloader
 // @version      1.35.2
-// @description  Download E-Hentai archive as zip file
+// @description  Download E-Hentai archive as zip file (with x-info patch by dnsev-h)
 // @author       864907600cc
 // @icon         https://secure.gravatar.com/avatar/147834caf9ccb0a66b2505c753747867
 // @include      http://exhentai.org/g/*
@@ -14,8 +14,8 @@
 // @include      https://g.e-hentai.org/g/*
 // @include      https://r.e-hentai.org/g/*
 // @namespace    http://ext.ccloli.com
-// @updateURL    https://github.com/ccloli/E-Hentai-Downloader/raw/master/e-hentai-downloader.meta.js
-// @downloadURL  https://github.com/ccloli/E-Hentai-Downloader/raw/master/e-hentai-downloader.user.js
+// @updateURL    https://github.com/ccloli/E-Hentai-Downloader/raw/dnsev-h_x-info/e-hentai-downloader.meta.js
+// @downloadURL  https://github.com/ccloli/E-Hentai-Downloader/raw/dnsev-h_x-info/e-hentai-downloader.user.js
 // @supportURL   https://github.com/ccloli/E-Hentai-Downloader/issues
 // @connect      e-hentai.org
 // @connect      exhentai.org
